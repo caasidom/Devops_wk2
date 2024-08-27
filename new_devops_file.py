fb4057bdf00259e13863a2aa5a46719b06f019da
caasidom <35532373+caasidom@users.noreply.github.com>
2024-08-27 13:21:00 +0100

    first_devop

---
+++ new_devops_file.py
@@ -0,0 +1,6 @@
+# Function to multiply two numbers to display the outcome of the numbers
+def multiply_numbers(num1=30,num2=40):
+        multiply=num1 *num2
+        print(multiply)
+multiply_numbers()
+        
